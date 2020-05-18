cc25e1ae1f525574279b974dccbe251c57526dea
Daniel Szemerey <szemy2@gmail.com>
2020-05-18 17:36:44 +0200

    Added slider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,19 @@
 import React, { Component } from 'react';
-import Sketch from "react-p5";
+
+import { Cubes } from './Framework/Cubes';
+import { Triangles } from './Framework/Triangles';
 
 
-import { util } from './Params/util';
-import { Population } from './Engine2/Population';
 
-// import { inconsolata } from './Assets/InconsolataCondensed.ttf'
-
-let myFont: any;
-
-
-interface MyProps {
-
-}
-
-interface MyState {
-  running: boolean,
-  showAll: boolean,
-  pop: Population,
-  bestFitness: number
-}
-
-
-export default class App extends Component<MyProps, MyState> {
-
-  myFont: any = 0;
-
-  constructor(props: any) {
-    super(props);
-    this.state = { running: false, showAll: false, pop: new Population(), bestFitness: 0 }
-  }
-
-  setup = (p: any, canvasParentRef: any) => {
-    p.createCanvas(util.canvasWidth, util.canvasHeight, p.WEBGL).parent(canvasParentRef); // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
-    // p.textFont(myFont)
-    this.resetSketch();
-  };
-
-  draw = (p: any) => {
-    const { showAll, pop } = this.state;
-
-    if (p.frameCount % 10 === 0) {
-      pop.evolve();
-      // console.log(this.pop.m_pop[this.pop.m_pop.length - 1].m_fitness)
-    }
-
-    p.background(205);
-    p.noStroke();
-    p.fill(255);
-    p.lights();
-
-    if (showAll) {
-      for (let i = 0; i < pop.m_pop.length; i++) {
-        p.push();
-        p.translate(-p.width / 2, -p.height / 2, 0);
-        p.scale(0.1, 0.1, 0.1);
-        p.translate(p.width * (i % 10), p.height * (i / 10), 0);
-        p.rotateY(0.01 * p.frameCount);
-        pop.m_pop[i].draw(p);
-        p.pop();
-      }
-    }
-    else {
-      p.push();
-      p.rotateY(0.01 * p.frameCount);
-      pop.m_pop[pop.m_pop.length - 1].draw(p);
-      p.pop();
-    }
-    this.setState({bestFitness:pop.m_pop[pop.m_pop.length - 1].m_fitness});
-  };
-
-  resetSketch() {
-    this.setState({ pop: new Population() });
-  }
-
-  preload(p: any) {
-    // myFont = p.loadFont('./Assets/InconsolataCondensed');
-  }
+export default class App extends Component {
 
   render() {
-    const { running, showAll,  bestFitness } = this.state;
-
-    return <div>
-      <Sketch setup={this.setup} draw={running ? this.draw : () => ({})} preload={this.preload} />
-      <button onClick={() => this.setState({ running: !running })}>{running ? 'Pause' : 'Run'}</button>
-      <button onClick={() => this.setState({ showAll: !showAll })}>{showAll ? 'Single' : 'All'}</button>
-      <button onClick={() => this.resetSketch()}>Reset</button>
-      <div> {bestFitness}</div>
-    </div>
+    return (
+      <div style={{ display: 'flex' }}>
+        <Cubes />
+        <Triangles />
+      </div>
+    )
   }
 }
 
